604d2b69aaa8463e9c9b8eab39baf61cb333aa84
Vital Ozierski <v.ozierski@ozitag.com>
2020-07-14 14:09:31 +0300

    add routes

--- routes/routes.php
+++ routes/routes.php
@@ -4,3 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/tager/settings', \OZiTAG\Tager\Backend\Settings\Controllers\PublicController::class . '@view');
 
+Route::group(['prefix' => 'admin', 'middleware' => ['passport:administrators', 'auth:api']], function () {
+    Route::get('/settings', \OZiTAG\Tager\Backend\Settings\Controllers\AdminController::class . '@index');
+    Route::get('/settings/{id}', \OZiTAG\Tager\Backend\Settings\Controllers\AdminController::class . '@view');
+    Route::put('/settings/{id}', \OZiTAG\Tager\Backend\Settings\Controllers\AdminController::class . '@update');
+});
