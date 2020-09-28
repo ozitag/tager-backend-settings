0ecc7faa6f08c615b07ae577829d192870e24dda
Vital Ozierski <v.ozierski@ozitag.com>
2020-09-28 16:28:25 +0300

    improve repeater public value

--- src/Features/ViewSettingsFeature.php
+++ src/Features/ViewSettingsFeature.php
@@ -4,8 +4,11 @@ namespace OZiTAG\Tager\Backend\Settings\Features;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use OZiTAG\Tager\Backend\Core\Features\Feature;
+use OZiTAG\Tager\Backend\Fields\Fields\RepeaterField;
 use OZiTAG\Tager\Backend\Fields\TypeFactory;
+use OZiTAG\Tager\Backend\Fields\Types\RepeaterType;
 use OZiTAG\Tager\Backend\Settings\Repositories\SettingsRepository;
+use OZiTAG\Tager\Backend\Settings\Utils\TagerSettingsConfig;
 
 class ViewSettingsFeature extends Feature
 {
@@ -15,7 +18,18 @@ class ViewSettingsFeature extends Feature
 
         $result = [];
         foreach ($items as $item) {
+
             $type = TypeFactory::create($item->type);
+            if ($type instanceof RepeaterType) {
+                $configField = TagerSettingsConfig::getField($item->key);
+                if (!$configField) continue;
+
+                /** @var RepeaterField $field */
+                $field = $configField->getField();
+
+                $type->setFields($field->getFields());
+            }
+
             $type->loadValueFromDatabase($item->value);
 
             $result[] = [
