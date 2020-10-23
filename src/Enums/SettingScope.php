8da625aa117db1f22748046debcec4c4d1f1293f
Vital Ozierski <v.ozierski@ozitag.com>
2020-10-23 12:33:01 +0300

    add scopes

---
+++ src/Enums/SettingScope.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OZiTAG\Tager\Backend\Settings\Enums;
+
+use OZiTAG\Tager\Backend\Core\Enums\Enum;
+
+final class SettingScope extends Enum
+{
+    const View = 'settings.view';
+    const Edit = 'settings.edit';
+}
--- src/SettingsServiceProvider.php
+++ src/SettingsServiceProvider.php
@@ -3,7 +3,9 @@
 namespace OZiTAG\Tager\Backend\Settings;
 
 use Illuminate\Support\ServiceProvider;
+use OZiTAG\Tager\Backend\Rbac\TagerScopes;
 use OZiTAG\Tager\Backend\Settings\Console\FlushSettingsCommand;
+use OZiTAG\Tager\Backend\Settings\Enums\SettingScope;
 
 class SettingsServiceProvider extends ServiceProvider
 {
@@ -38,6 +40,11 @@ class SettingsServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__ . '/../config.php' => config_path('tager-settings.php'),
+        ]);
+
+        TagerScopes::registerGroup('Settings', [
+            SettingScope::View => 'View settings',
+            SettingScope::Edit => 'Edit settings'
         ]);
     }
 }
