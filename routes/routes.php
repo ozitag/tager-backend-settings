66a5650e4c4514f6a3c60977d6d278c9b38f2f14
Vital Ozierski <v.ozierski@ozitag.com>
2020-10-29 02:10:48 +0300

    add rbac support

--- routes/routes.php
+++ routes/routes.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use OZiTAG\Tager\Backend\Rbac\Facades\AccessControlMiddleware;
+use OZiTAG\Tager\Backend\Settings\Enums\SettingScope;
 
 
 Route::group(['middleware' => 'api.cache'], function () {
@@ -8,7 +10,12 @@ Route::group(['middleware' => 'api.cache'], function () {
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => ['passport:administrators', 'auth:api']], function () {
-    Route::get('/settings', [\OZiTAG\Tager\Backend\Settings\Controllers\AdminController::class, 'index']);
-    Route::get('/settings/{id}', [\OZiTAG\Tager\Backend\Settings\Controllers\AdminController::class, 'view']);
-    Route::put('/settings/{id}', [\OZiTAG\Tager\Backend\Settings\Controllers\AdminController::class, 'update']);
+    Route::group(['middleware' => [AccessControlMiddleware::scopes(SettingScope::View)]], function () {
+        Route::get('/settings', [\OZiTAG\Tager\Backend\Settings\Controllers\AdminController::class, 'index']);
+        Route::get('/settings/{id}', [\OZiTAG\Tager\Backend\Settings\Controllers\AdminController::class, 'view']);
+    });
+
+    Route::group(['middleware' => [AccessControlMiddleware::scopes(SettingScope::Edit)]], function () {
+        Route::put('/settings/{id}', [\OZiTAG\Tager\Backend\Settings\Controllers\AdminController::class, 'update']);
+    });
 });
