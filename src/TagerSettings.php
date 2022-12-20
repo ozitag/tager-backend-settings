c27cafcae68408b7a71ce92a1c9dc4051edf8f4d
Vital Ozierski <vital@ozerich.com>
2022-12-20 13:26:01 +0300

    1.9.3 - add getPublicValue for TagerSettings class

--- src/TagerSettings.php
+++ src/TagerSettings.php
@@ -3,8 +3,10 @@
 namespace OZiTAG\Tager\Backend\Settings;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use OZiTAG\Tager\Backend\Fields\Base\Type;
 use OZiTAG\Tager\Backend\Fields\Enums\FieldType;
 use OZiTAG\Tager\Backend\Fields\TypeFactory;
+use OZiTAG\Tager\Backend\Fields\Types\RepeaterType;
 use OZiTAG\Tager\Backend\Settings\Jobs\GetSettingByKeyJob;
 use OZiTAG\Tager\Backend\Settings\Jobs\UpdateSettingValueJob;
 use OZiTAG\Tager\Backend\Settings\Utils\TagerSettingsConfig;
@@ -13,14 +15,24 @@ class TagerSettings
 {
     use DispatchesJobs;
 
-    public function get($key)
+    private function loadType(string $key): ?Type
     {
         $model = $this->dispatch(new GetSettingByKeyJob($key));
         if (!$model) {
             return null;
         }
 
-        $type = TypeFactory::create(FieldType::from($model->type));
+        if ($model->type == FieldType::Repeater->value) {
+            $settingsField = TagerSettingsConfig::getField($key);
+            if ($settingsField) {
+                $type = $settingsField->getField()->getTypeInstance();
+            } else {
+                return null;
+            }
+        } else {
+            $type = TypeFactory::create(FieldType::from($model->type));
+        }
+
         $type->loadValueFromDatabase($model->value);
 
         if (!empty($type->hasFiles())) {
@@ -30,7 +42,28 @@ class TagerSettings
             }
         }
 
+        return $type;
+    }
+
+    public function get($key)
+    {
+        $type = $this->loadType($key);
+        if (!$type) {
+            return null;
+        }
+
         return $type->getValue();
+    }
+
+
+    public function getPublicValue($key)
+    {
+        $type = $this->loadType($key);
+        if (!$type) {
+            return null;
+        }
+
+        return $type->getPublicValue();
     }
 
     public function set($key, $value)
